7bacf0d41c301a4a7793dc5ce50c09ac35d7a8fc
fl-fabio <fl.fabio.lanzafame@gmail.com>
2026-04-15 12:14:04 +0200

    feature: add error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,24 @@
 import { Routes } from '@angular/router';
 import { guestGuard } from './core/guards/guest.guard';
+import { LoginPage } from './auth/pages/login-page/login-page';
+import { RegisterPage } from './auth/pages/register-page/register-page';
+import { ErrorPage } from './pages/error-page/error-page';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'users'
+    redirectTo: 'login'
   },
   {
     path: 'login',
     canActivate: [guestGuard],
-    loadComponent: () =>
-      import('./auth/pages/login-page/login-page').then((m) => m.LoginPage)
+    component: LoginPage
   },
   {
     path: 'register',
     canActivate: [guestGuard],
-    loadComponent: () =>
-      import('./auth/pages/register-page/register-page').then((m) => m.RegisterPage)
+    component: RegisterPage
   },
   {
     path: 'users',
@@ -25,7 +26,11 @@ export const routes: Routes = [
       import('./routes/users.routes').then((m) => m.USERS_ROUTES)
   },
   {
+    path: '404',
+    component: ErrorPage
+  },
+  {
     path: '**',
-    redirectTo: 'users'
+    redirectTo: '404'
   }
 ];
