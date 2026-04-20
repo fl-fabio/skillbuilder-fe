05cbdd6b4271c8b9324a89f5a19f84aed9b7603a
Samuel Okotako <francissamuel840@gmail.com>
2026-04-20 11:59:18 +0200

    chore(config): add environment-based API configuration

--- src/app/services/skills-evaluation.api.service.ts
+++ src/app/services/skills-evaluation.api.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Skill } from '../models/skill.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SkillsEvaluationApiService {
   http = inject(HttpClient);
-  baseUrl = 'http://82.165.174.28/api';
+  baseUrl = environment.apiBaseUrl;
 
   getSkillsForJobTitle(jobTitleId: string): Observable<Skill[]> {
     return this.http.get<Skill[]>(`${this.baseUrl}/data/skills/${jobTitleId}`);  
